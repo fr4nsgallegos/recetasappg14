779ddee1139b8a802fad789e456df6cedd85cdf4
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2025-11-26 20:31:45 -0500

    vlidando correo

--- lib/examples/textfield_page.dart
+++ lib/examples/textfield_page.dart
@@ -1,8 +1,22 @@
 import 'package:flutter/material.dart';
 
-class TextfieldPage extends StatelessWidget {
+class TextfieldPage extends StatefulWidget {
   TextfieldPage({super.key});
+
+  @override
+  State<TextfieldPage> createState() => _TextfieldPageState();
+}
+
+class _TextfieldPageState extends State<TextfieldPage> {
   TextEditingController _emailController = TextEditingController();
+
+  bool _hasError = false;
+
+  void _validarCorreo() {
+    String correo = _emailController.text;
+    _hasError = !correo.contains("@");
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -24,24 +38,29 @@ class TextfieldPage extends StatelessWidget {
                   fontWeight: FontWeight.bold,
                 ),
                 cursorColor: Colors.red, //Color de cursos
-                cursorWidth: 20, //ancho del cursor
-                cursorHeight: 40, //alto del cursor
+                cursorWidth: 10, //ancho del cursor
+                cursorHeight: 20, //alto del cursor
                 cursorRadius: Radius.circular(
                   2,
                 ), //border redondeados en el cursos
-                maxLines: 2, //máximo de lineas en el textfield
+                maxLines: 1, //máximo de lineas en el textfield
+                onChanged: (value) => _validarCorreo(),
                 textCapitalization: TextCapitalization.words,
                 decoration: InputDecoration(
+                  errorText: _hasError ? "Formato inválido" : null,
                   filled: true,
                   fillColor: Colors.grey.shade300,
                   labelText: "Correo",
                   hintText: "Ejemplo@gmail.com",
                   helperText: "Ingresa un correo válido",
                   prefixIcon: Icon(Icons.person),
-                  suffixIcon: Icon(Icons.check_circle),
+                  suffixIcon: _hasError
+                      ? Icon(Icons.cancel)
+                      : Icon(Icons.check_circle),
+                  suffixIconColor: _hasError ? Colors.red : Colors.green,
                   // border: UnderlineInputBorder(), //linea inferior
                   border: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.red, width: 16),
+                    borderSide: BorderSide(color: Colors.green, width: 4),
                     // borderSide: BorderSide.none,
                   ),
                   enabledBorder: OutlineInputBorder(
@@ -50,10 +69,21 @@ class TextfieldPage extends StatelessWidget {
                     borderRadius: BorderRadius.circular(25),
                   ),
                   focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.green, width: 4),
+                    borderRadius: BorderRadius.circular(25),
+                  ),
+                  focusedErrorBorder: OutlineInputBorder(
                     borderSide: BorderSide(color: Colors.red, width: 4),
                     borderRadius: BorderRadius.circular(25),
                   ),
                 ),
+              ),
+              SizedBox(height: 32),
+              ElevatedButton(
+                onPressed: () {
+                  _validarCorreo();
+                },
+                child: Text("Validar"),
               ),
             ],
           ),
