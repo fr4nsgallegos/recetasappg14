9be7c3d2c33dc81c65b705b1f1c4b153f6664ee7
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2025-11-26 20:20:17 -0500

    init text field

---
+++ lib/examples/textfield_page.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+class TextfieldPage extends StatelessWidget {
+  TextfieldPage({super.key});
+  TextEditingController _emailController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Text Field Page")),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(32.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextField(
+                controller: _emailController,
+                keyboardType: TextInputType.emailAddress, //tipo de teclado
+                // obscureText: true, //ocultar texto como contraseñas
+                style: TextStyle(
+                  color: Colors.blue,
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+                cursorColor: Colors.red, //Color de cursos
+                cursorWidth: 20, //ancho del cursor
+                cursorHeight: 40, //alto del cursor
+                cursorRadius: Radius.circular(
+                  2,
+                ), //border redondeados en el cursos
+                maxLines: 2, //máximo de lineas en el textfield
+                textCapitalization: TextCapitalization.words,
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.grey.shade300,
+                  labelText: "Correo",
+                  hintText: "Ejemplo@gmail.com",
+                  helperText: "Ingresa un correo válido",
+                  prefixIcon: Icon(Icons.person),
+                  suffixIcon: Icon(Icons.check_circle),
+                  // border: UnderlineInputBorder(), //linea inferior
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.red, width: 16),
+                    // borderSide: BorderSide.none,
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    // borderSide: BorderSide(color: Colors.blueAccent, width: 4),
+                    borderSide: BorderSide.none,
+                    borderRadius: BorderRadius.circular(25),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.red, width: 4),
+                    borderRadius: BorderRadius.circular(25),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:recetasappg14/examples/textfield_page.dart';
 import 'package:recetasappg14/pages/home_page.dart';
 
 void main() {
-  runApp(MaterialApp(home: HomePage(), debugShowCheckedModeBanner: false));
+  runApp(MaterialApp(home: TextfieldPage(), debugShowCheckedModeBanner: false));
 }
