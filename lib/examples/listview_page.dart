fe73c06e9d9f6c3a51b8a5040e7e43ba99beb270
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2025-11-27 20:19:34 -0500

    listview

---
+++ lib/examples/listview_page.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class ListviewPage extends StatelessWidget {
+  const ListviewPage({super.key});
+
+  Widget _buildContainers(String text) {
+    return Container(
+      height: 50,
+      width: 200,
+      color: Colors.red,
+      margin: EdgeInsets.all(16),
+      child: Center(child: Text(text)),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView.builder(
+        itemCount: 1000,
+        itemBuilder: (BuildContext context, int index) {
+          print("MOSTRANDO: $index");
+          return _buildContainers(index.toString());
+        },
+      ),
+      // ListView(
+      //   children: [
+      //     ...List.generate(50, (index) => _buildContainers(index.toString())),
+      //   ],
+      // ),
+    );
+  }
+}
